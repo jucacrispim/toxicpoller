c594460de46f8f244f6ba4ea705680cb69540286
Juca Crispim <juca@poraodojuca.dev>
2025-10-09 23:41:38 -0300

    Fix linter checks

--- scripts/run_linter.sh
+++ scripts/run_linter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 pylint toxicpoller/
-if [ $? != "0" ]
+if [ "$?" != "0" ]
 then
     exit 1;
 fi
 
 flake8 toxicpoller/
 
-if [ $? != "0" ]
+if [ "$?" != "0" ]
 then
     exit 1;
 fi
